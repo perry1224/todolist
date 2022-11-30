21da5aa7c8db5a934601c53fb8ec8631cc6ab346
Perry <perry.w1224@gmail.com>
2022-11-30 16:49:04 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -15,23 +15,15 @@ window.onload = function(){
         `;
     }
 
-    var current_tasks = document.querySelectorAll(".delete"); 
-    for (let i=0; i<current_tasks.length; i++){
-       current_tasks[i].onclick = function() {
-           this.parentNode.remove();
-       }
-   }
-    
-    
-    
+   
+        
     
     // console.log(items);
     let getData = [];
     document.querySelector('.add').onclick = function() {
         if(document.querySelector('#newtask input').value.length == 0) {
             alert("Please enter a task")
-        }
-        else {
+        } else {
             document.querySelector('.tasks-group').innerHTML +=`
             <div class="task">
                 <span class="taskname"> 
@@ -56,23 +48,28 @@ window.onload = function(){
             localStorage.setItem(getData[i], i);
             
         }
+            }
+        }   
+    //         var current_tasks = document.querySelectorAll(".delete"); 
+    //     for (let i=0; i<current_tasks.length; i++){
+    //        current_tasks[i].onclick = function() {
+    //            this.parentNode.remove();
+    //            console.log('.delete')
+             
+    //        }
+    //    }
+
+
+
+
+       
+
+
     
-    //  var current_tasks = document.querySelectorAll(".delete"); 
-    //  for (let i=0; i<current_tasks.length; i++){
-    //     current_tasks[i].onclick = function() {
-    //         this.parentNode.remove();
-    //     }
-        
-    //  }
-    
+        function myFunction() {
+            var element = document.getElementById("tasks");
+            element.classList.toggle("mystyle");
         }
-    }
-    
-    
-    function myFunction() {
-        var element = document.getElementById("tasks");
-        element.classList.toggle("mystyle");
-     }
     
      Sortable.create(list1, {
         animation: 120,
@@ -83,7 +80,16 @@ window.onload = function(){
         chosenClass: 'active'
       });
     
+      
     }
     
+    document.addEventListener('click', function (event) {
+        // If the clicked element doesn't have the right selector, bail
+        if (!event.target.matches('.delete')) return;
+        // Don't follow the link
+        event.preventDefault();
+        // Log the clicked element in the console
+        console.log(event.target);
+    }, false);
     
     
